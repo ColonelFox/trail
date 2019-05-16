3592bac2ae8830c5ea34b551ffb938c67971abca
DavidSommerhalder <da.air@gmx.ch>
2019-05-16 23:59:56 +0200

    Added steps

--- script.js
+++ script.js
@@ -4,9 +4,46 @@
 var failCtr = 0;
 
 function btnReadyClick() {
-    console.log("Hallo");
+    unhide("giraffe");
+    unhide("step1");
     window.scrollBy(0, 100000);
 }
+
+function btnNotReadyClick() {
+    alert("Oooch... vilech luegsch nomal ds Video, u atmisch töif düre... If that doesn't help, maybe call me? But only if you're really really really desperate ;)");
+}
+
+function btnCheckBuslineClick() {
+    
+    let phrase = window.document.getElementById("busline");
+    let text = phrase.value;
+    console.log(text);
+
+    if(text == "1") {
+        unhide("step2");
+        failCtr = 0;
+    }
+    else {
+        if(failCtr == 0) {
+            alert("Leider nein... 1. Hiiwiis: Es isch ä Zahl...");
+            failCtr++;
+        }
+        else if(failCtr == 1) {
+            alert("Immer noni ;) 2. Hiiwiis: Es isch ä Zahl zwüsche 1 und 10");
+            failCtr++;
+        }
+        else if(failCtr == 2) {
+            alert("Hmmm, nope... 3. Hiiwiis: Es isch en ungeradi Zahl, chliner als 5...");
+            failCtr++;
+        }
+        else {
+            alert("Schad... het äue öbbis nid ganz klappet... darfsch glich witers ;)");
+            unhide("step2");
+            failCtr = 0;
+        }
+    }
+}
+
 
 function btnCheckClick() {
     
@@ -38,4 +75,9 @@ function btnCheckClick() {
 function unhideSecret() {
     let secret = window.document.getElementById("secret");
     secret.style.visibility = "visible";
+}
+
+function unhide(id) {
+    let el = window.document.getElementById(id);
+    el.style.visibility = "visible";
 }
